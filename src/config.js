cb04cd6d11f1971be13fccda268b993025510149
NotAvalible111 <mark.pezo30@gmail.com>
2024-12-30 14:36:31 -0500

    config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
 module.exports = {
 
+    // BOT INFO //
+    prefix: process.env.prefix || '!!',
+    ownerID: '752340405688729650', // Tu ID de usuario
+
+    // COLORS //
+    embedColor: '#2f3136', // Color predeterminado para embeds
+
+    // CHANNEL IDS //
+    logschannel: "1246682278776799253", // slash command canal para logs
+
 };
